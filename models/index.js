cb2807471a879a2394684c7e45a264193701cd8f
ehui eric <ehui_eric@yahoo.fr>
2021-03-23 00:10:50 -0400

    update

--- models/index.js
+++ models/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-    Plant: require("./workout"),
+    Workout: require("./Workout"),
   };
--- server.js
+++ server.js
@@ -4,20 +4,17 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 // const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-//app.use(routes);
+app.use(express.static("public"));
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout");
+app.use(require("./routes/api-routes.js"));
+require("./routes/html-routes.js")(app);
 
 // Start the API server
 app.listen(PORT, function() {
